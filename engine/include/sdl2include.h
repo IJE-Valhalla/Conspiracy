c54392f37aad476cdbf5cb62921655480c851e2a
Rodrigo <rodrigo.redcode@gmail.com>
2017-04-22 18:21:23 -0300

    Edit project structure

--- engine/include/sdl2include.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENGINE_SDL2_INCLUDES_HPP
-#define ENGINE_SDL2_INCLUDES_HPP
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
-
-#endif
