2ffd3a1691d52f3f4d51fe98321c68e826c911da
pablodiegoss <pablodiegoss@hotmail.com>
2017-04-26 17:30:38 -0300

    Refactor Engine: Create Window and SDL Managers

--- engine/include/engine.hpp
+++ engine/include/engine.hpp
@@ -1,19 +1,13 @@
 #ifndef ENGINE_HPP
 #define ENGINE_HPP
 
-#include <sdl2includes.hpp>
-#include <string>
 #include <utility>
 #include "log.h"
+#include "window_manager.hpp"
+#include "sdl_manager.hpp"
 
 namespace engine{
-
-  bool initSDL();
-  bool finalizeSDL();
-  bool createWindow(std::string windowTitle, std::pair<int, int> windowSize);
-  bool destroyWindow();
   void run();
   void loadEngine();
-  SDL_Window* GetGameWindow();
 }
 #endif
---
+++ engine/include/window_manager.hpp
@@ -0,0 +1,16 @@
+#ifndef WINDOW_MANAGER_HPP
+#define WINDOW_MANAGER_HPP
+
+#include <string>
+#include <sdl2includes.hpp>
+#include "log.h"
+
+namespace engine{
+    class WindowManager{
+    public:
+        bool createWindow(std::string windowTitle, std::pair<int, int> windowSize);
+        bool destroyWindow();
+        SDL_Window* GetGameWindow();
+    };
+}
+#endif
