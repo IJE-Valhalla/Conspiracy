43cbd393029561a2d5ae54fe9f77e279e8140f02
pablodiegoss <pablodiegoss@hotmail.com>
2017-04-26 17:34:43 -0300

    Refactor window and sdl managers

--- engine/src/window_manager.cpp
+++ engine/src/window_manager.cpp
@@ -5,44 +5,44 @@ SDL_Window *gameWindow;
 SDL_Renderer *gameCanvas;
 
 bool WindowManager::createWindow(std::string windowTitle, std::pair<int, int> windowSize){
-  INFO("Create window");
-  gameWindow = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED,
-                                SDL_WINDOWPOS_CENTERED, windowSize.first,
-                                windowSize.second, SDL_WINDOW_SHOWN);
+    INFO("Create window");
+    gameWindow = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, windowSize.first,
+    windowSize.second, SDL_WINDOW_SHOWN);
+    
+    if(gameWindow == NULL){
+        return false;
+    }
 
-  if(gameWindow == NULL){
-    return false;
-  }
+    INFO("Create canvas");
+    gameCanvas = SDL_CreateRenderer(gameWindow, -1,
+        SDL_RENDERER_ACCELERATED |
+        SDL_RENDERER_PRESENTVSYNC);
 
-  INFO("Create canvas");
-  gameCanvas = SDL_CreateRenderer(gameWindow, -1,
-                                  SDL_RENDERER_ACCELERATED |
-                                  SDL_RENDERER_PRESENTVSYNC);
+        if(gameCanvas == NULL){
+            return false;
+        }
 
-  if(gameCanvas == NULL){
-    return false;
-  }
+        SDL_SetRenderDrawColor(gameCanvas, 0, 0, 0, 0);
+        SDL_RenderClear(gameCanvas);
+        SDL_RenderPresent(gameCanvas);
 
-  SDL_SetRenderDrawColor(gameCanvas, 0, 0, 0, 0);
-  SDL_RenderClear(gameCanvas);
-  SDL_RenderPresent(gameCanvas);
-
-  return true;
-}
+        return true;
+    }
 
 
-bool WindowManager::destroyWindow(){
-  INFO("Destroying canvas");
-  SDL_DestroyRenderer(gameCanvas);
-  gameCanvas = NULL;
+    bool WindowManager::destroyWindow(){
+        INFO("Destroying canvas");
+        SDL_DestroyRenderer(gameCanvas);
+        gameCanvas = NULL;
 
-  INFO("Destroying window");
-  SDL_DestroyWindow(gameWindow);
-  gameWindow = NULL;
+        INFO("Destroying window");
+        SDL_DestroyWindow(gameWindow);
+        gameWindow = NULL;
 
-  return true;
-}
+        return true;
+    }
 
-SDL_Window* GetGameWindow(){
-    return gameWindow;
-}
+    SDL_Window* GetGameWindow(){
+        return gameWindow;
+    }
