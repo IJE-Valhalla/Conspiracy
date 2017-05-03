408870dbd87bba658a8345b368de58a5bbbf9ec7
pablodiegoss <pablodiegoss@hotmail.com>
2017-05-03 20:32:12 -0300

    Fix audio bugs

--- engine/include/audio.hpp
+++ engine/include/audio.hpp
@@ -1,18 +1,21 @@
 #ifndef AUDIO_HPP
 #define AUDIO_HPP
 
+#include "sdl2includes.hpp"
+
 namespace engine{
 
     class Audio{
     public:
-        Audio(const std::string audioPath, const std::string audioType);
+        Audio(std::string audioPath, std::string audioType);
         ~Audio();
-        play(const int timesToPlay);
-        pause();
-        resume();
-        stop();
+        void play(const int timesToPlay);
+        void pause();
+        void resume();
+        void stop();
     private:
         Mix_Music* audioMusic = NULL;
         Mix_Chunk* audioEffect = NULL;
     };
 }
+#endif
