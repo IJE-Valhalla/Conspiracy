1a5bdae0c7c94d7559c844c030698e5a09e33168
pablodiegoss <pablodiegoss@hotmail.com>
2017-05-25 16:17:00 -0300

    Change Alien update method to allow collision

--- game/src/alien.cpp
+++ game/src/alien.cpp
@@ -13,35 +13,37 @@ Alien::~Alien(){}
 
 void Alien::update(double timeElapsed){
     // To Do: Use Time Elapsed in inc.
+    auto incX = 0.15*timeElapsed;
+    auto incY = 0.15*timeElapsed;
     if(InputManager::instance.isKeyPressed(InputManager::KeyPress::KEY_PRESS_RIGHT)){
-        auto inc = 0.15*timeElapsed;
-        setPositionX(getPositionX()+inc);
+        incX = incX;
         animator->setInterval(0,3);
     }
     else if(InputManager::instance.isKeyPressed(InputManager::KeyPress::KEY_PRESS_LEFT)){
-        auto inc = 0.15*timeElapsed;
-        setPositionX(getPositionX()-inc);
-        animator->setInterval(0,3);
-    }
-
-    else if(InputManager::instance.isKeyPressed(InputManager::KeyPress::KEY_PRESS_UP)){
-        auto inc = 0.15*timeElapsed;
-        WARN("INCREMENTO//:" << inc <<"POSITION" << getPositionY());
-        setPositionY(getPositionY()-inc);
-        WARN("INCREMENTO//:" << inc <<"POSITION" << getPositionY());
-        animator->setInterval(0,3);
-    }
-    else if(engine::InputManager::instance.isKeyPressed(engine::InputManager::KeyPress::KEY_PRESS_DOWN)){
-        auto inc = 0.15*timeElapsed;
-        WARN("INCREMENTO//:" << inc <<"POSITION" << getPositionY());
-        setPositionY(getPositionY()+inc);
-        WARN("INCREMENTO//:" << inc <<"POSITION" << getPositionY());
+        incX = incX * (0-1);
         animator->setInterval(0,3);
     }
     else {
+        incX = 0;
+    }
+    setPositionX(getPositionX()+incX);
+
+    if(InputManager::instance.isKeyPressed(InputManager::KeyPress::KEY_PRESS_UP)){
+        incY = incY * (0-1);
+        animator->setInterval(0,3);
+    }
+    else if(engine::InputManager::instance.isKeyPressed(engine::InputManager::KeyPress::KEY_PRESS_DOWN)){
+        incY = incY;
+        animator->setInterval(0,3);
+    }
+    else {
+        incY = 0;
+    }
+    setPositionY(getPositionY()+incY);
+
+    if(incX == 0 && incY == 0){
         animator->setInterval(animator->getInterval().first, animator->getInterval().first);
     }
-
     animator->update();
 }
 
