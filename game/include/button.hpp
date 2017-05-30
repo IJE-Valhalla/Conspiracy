6ed631a37e8762d0ed4786b74c8771b0a23b7507
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-05-30 20:03:25 -0300

    Adding button structure.

---
+++ game/include/button.hpp
@@ -0,0 +1,22 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+#include "engine.hpp"
+#include "input_manager.hpp"
+#include "game_object.hpp"
+#include "text.hpp"
+
+using namespace engine;
+
+class Button: public GameObject{
+public:
+    Button(std::string objectName, double positionX, double positionY, int width, int height, std::string title, int size);
+    ~Button();
+    void update(double timeElapsed);
+    void draw();
+    void init();
+protected:
+    Text * text;
+};
+
+#endif
