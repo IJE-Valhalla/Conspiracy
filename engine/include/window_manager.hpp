794d4c7f4c52e10a30696fe26c263a71f4978770
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-06-30 20:20:00 -0300

    Adding image icon in window.

--- engine/include/window_manager.hpp
+++ engine/include/window_manager.hpp
@@ -12,6 +12,8 @@ namespace engine{
         bool destroyWindow();
         SDL_Window* GetGameWindow();
         static SDL_Renderer* getGameCanvas();
+    private:
+        void setImageIcon();
     };
 }
 #endif
