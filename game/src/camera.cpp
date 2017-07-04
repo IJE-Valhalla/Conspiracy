f7a2d3b5565710e890dc24d5847871af585b3ee9
pablodiegoss <pablodiegoss@hotmail.com>
2017-07-03 22:44:21 -0300

    Create cameras at stage 2
    Fix stage 1 camera

--- game/src/camera.cpp
+++ game/src/camera.cpp
@@ -1,19 +1,27 @@
 #include "camera.hpp"
 
 Camera::Camera(std::string objectName, double positionX, double positionY,
-               int width, int height) : Enemy(objectName,
+               int width, int height, std::string direction) : Enemy(objectName,
                                               positionX,
                                               positionY,
                                               width, height){
 
     animator = new Animation(objectName, 1, 4, 0.1);
     animator->addAction("right",0,0);
-    animator->setInterval("right");
-    range = 150;
-    int angleOfVision = 60;
+    animator->addAction("left",2,2);
 
-    fieldOfVision = new FieldOfVision(positionX+4+width/2,positionY+7, range, angleOfVision);
-    fieldOfVision->setAngle(305);
+    animator->setInterval(direction);
+    range = 180;
+    int angleOfVision = 80;
+    int centralizeVisionX = 4;
+    if(direction == "right"){
+        fieldOfVision = new FieldOfVision(positionX+centralizeVisionX+width/2,positionY+7, range, angleOfVision);
+        fieldOfVision->setAngle(305);
+    }else{
+        fieldOfVision = new FieldOfVision(positionX-centralizeVisionX+width/2,positionY+7, range, angleOfVision);
+        fieldOfVision->setAngle(220);
+    }
+
 }
 
 Camera::~Camera(){
