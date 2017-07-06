d54f28479845d6414928ad0d0ddba1032b9fc264
Rodrigo <rodrigo.redcode@gmail.com>
2017-07-06 19:00:33 -0300

    Add new tutorial

--- game/include/tutorial_scene.hpp
+++ game/include/tutorial_scene.hpp
@@ -3,19 +3,22 @@
 
 #include "engine.hpp"
 #include "button.hpp"
+#include "timer.hpp"
 
 
 using namespace engine;
 
 class TutorialScene: public Scene{
 public:
-    TutorialScene(int id, std::string backgroundFile);
+    TutorialScene(int id, std::string backgroundFile, int nextScene);
     void load();
     void update(double timeElapsed);
     void draw();
     void unload();
 private:
+    Timer* sceneTimer;
     Sprite* background;
+    int newNextScene;
     std::vector<std::pair<int, GameObject*>> gameObjectsList;
 };
 
