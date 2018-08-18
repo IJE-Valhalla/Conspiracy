2a1733bcbf9619bd1082645f8a4377f14b436160
rodrigocam <rodrigo.redcode@gmail.com>
2018-08-18 15:20:43 -0300

    Refactor game_object

---
+++ code/engine/include/object.h
@@ -0,0 +1,57 @@
+/*
+* An object representation for use in a game.
+* All objects in a game should be represented by this class.
+*/
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <utility>
+
+class Object {
+
+    public:
+        Object(double x = 0, double y = 0, double w = 1, double h = 1);
+        virtual ~Object() = default;
+
+        virtual void update() = 0;
+        virtual void draw() = 0;
+
+        /* Getters */
+        std::pair<double, double> position() const;
+        std::pair<double, double> size() const;
+
+        bool is_active() const;
+        bool is_visible() const;
+
+        double x() const;
+        double y() const;
+
+        double w() const;
+        double h() const;
+
+        /* Setters */
+        void set_position(std::pair<double, double> new_position);
+        void set_size(std::pair<double, double> new_size);
+
+        void set_x(double new_x);
+        void set_y(double new_y);
+
+        void set_w(double new_w);
+        void set_h(double new_h);
+
+        void set_active(bool status);
+        void set_visible(bool status);
+    
+    private:
+        bool active;
+        bool visible;
+
+        // position x and y
+        std::pair<double, double> m_position;
+        
+        // width and height
+        std::pair<double, double> m_size;
+};  
+
+#endif
---
+++ code/engine/src/object.cpp
@@ -0,0 +1,77 @@
+/*
+* Implementation of the class that represents an object
+* in a game.
+*/
+
+#include "object.h"
+#include "log.h"
+
+
+Object::Object(double x, double y, double w, double h) {
+    set_position(std::make_pair(x, y));
+    set_size(std::make_pair(w, h));
+}
+
+std::pair<double, double> Object::position() const {
+    return m_position;
+}
+
+std::pair<double, double> Object::size() const {
+    return m_size;
+}
+
+bool Object::is_active() const {
+    return active;
+}
+
+bool Object::is_visible() const {
+    return visible;
+}
+
+double Object::x() const {
+    return m_position.first;
+}
+
+double Object::y() const {
+    return m_position.second;
+}
+
+double Object::w() const {
+    return m_size.first;
+}
+
+double Object::h() const {
+    return m_size.second;
+}
+
+void Object::set_position(std::pair<double, double> new_position) {
+    m_position = new_position;
+}
+
+void Object::set_size(std::pair<double, double> new_size) {
+    m_size = new_size;
+}
+
+void Object::set_x(double new_x) {
+    m_position.first = new_x;
+}
+
+void Object::set_y(double new_y) {
+    m_position.second = new_y;
+}
+
+void Object::set_w(double new_w) {
+    m_size.first = new_w;
+}
+
+void Object::set_h(double new_h) {
+    m_size.second = new_h;
+}
+
+void Object::set_active(bool status) {
+    active = status;
+}
+
+void Object::set_visible(bool status) {
+    visible = status;
+}
